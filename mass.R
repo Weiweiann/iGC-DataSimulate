9bce095e827249e4342dc61c77fe1b1896045a04
Weian Wang <stargazer.wang@gmail.com>
2016-06-18 16:05:31 +0800

    igc can run the simulate data now

--- mass.R
+++ mass.R
@@ -2,65 +2,75 @@ require(MASS)
 library(data.table)
 
 
-dataSimulation <- function(n_people=1, n_genes=100, cnv_proportion=0.3, cnv_threhold = 2.5){
+dataSimulation <- function(n_people=100, n_genes=300, cnv_proportion=0.3, cnv_threhold = 2.5){
   Genes <- vector()
   CNVs <- vector()
 
-  for(i in 1:n_genes){
-  # simulate a gene
-    
   # setup correlation vector: random from 0~0.3 0.3~0.7 0.7~1
-  correlation = c(runif(1,0,0.3), runif(1,0.3,0.7),runif(1,0.7,1))
-  sample<-vector()
-  
-    for(cor in correlation){
-      
-      # cor_matirx is the matrix that fills with symmetry correlation value.
-      cor_matrix = diag(nrow = 2,ncol = 2)
-      cor_matrix[upper.tri(cor_matrix)] <- cor
-      cor_matrix[lower.tri(cor_matrix)] <- cor
-      
-      # mu is the mean of simulated cnv and gene
-      # the sd of "mvrnorm" is 1, thus we have to adjust the mean to set up the proportion of cnvs
-      # increase cnv_default_mean and calculate the threshold until it equals or larger to cnv_threhold
-   
-      cnv_normal_possibility = 1-cnv_proportion 
-      cnv_default_mean = 0
-      while(qnorm(cnv_normal_possibility, mean = cnv_default_mean, sd = 1) < cnv_threhold){
-        cnv_default_mean = cnv_default_mean + 0.05
-      }
-      
-      mu = c(cnv_threhold, 5) # the second one is the mean value for gene expression, it can be any number since it does not effect the simulation
-      
-      # simulate n_genes with sample correlation
-      sample_per_cor <- mvrnorm(n=n_people, mu = mu, Sigma = cor_matrix , empirical = T)
-      colnames(sample_per_cor) = c("CNV", "Gene")
-      sample <- rbind(sample,sample_per_cor)
-    }
-  
-  # combine with all the simulate reuslts.
-  CNVs <- cbind(CNVs, sample[,1])
-  Genes <- cbind(Genes, sample[,2])
+  if (n_genes %% 3 ==1){
+    correlation = c(runif(round(n_genes/3),0,0.3), runif(round(n_genes/3),0.3,0.7),runif(round(n_genes/3)+1,0.7,1))
+  }else if(n_genes %% 3 ==2 ){
+    correlation = c(runif(round(n_genes/3),0,0.3), runif(round(n_genes/3),0.3,0.7),runif(round(n_genes/3)+2,0.7,1))
+  }else{
+    correlation = c(runif(round(n_genes/3),0,0.3), runif(round(n_genes/3),0.3,0.7),runif(round(n_genes/3),0.7,1))
   }
   
-  Gene_name = paste('Gene',seq(1:(n_genes*length(correlation))))
+  for(i in 1:n_genes){
+    # simulate a gene
+    cor = correlation[i]
+   
+        
+    # cor_matirx is the matrix that fills with symmetry correlation value.
+    cor_matrix = diag(nrow = 2,ncol = 2)
+    cor_matrix[upper.tri(cor_matrix)] <- cor
+    cor_matrix[lower.tri(cor_matrix)] <- cor
+    
+    # mu is the mean of simulated cnv and gene
+    # the sd of "mvrnorm" is 1, thus we have to adjust the mean to set up the proportion of cnvs
+    # increase cnv_default_mean and calculate the threshold until it equals or larger to cnv_threhold
+    cnv_normal_possibility = 1-cnv_proportion 
+    cnv_default_mean = 0
+    while(qnorm(cnv_normal_possibility, mean = cnv_default_mean, sd = 1) < cnv_threhold){
+      cnv_default_mean = cnv_default_mean + 0.05
+    }
+  
+    # the second one is the mean value for gene expression, 
+    # it can be any number since it does not effect the simulation
+    mu = c(cnv_threhold, 5) 
+    
+    # simulate n_people with sample correlation
+    one_gene_simulate <- t(mvrnorm(n=n_people, mu = mu, Sigma = cor_matrix , empirical = T))
+    row.names(one_gene_simulate) = c("CNV", "GENE")
+    
+    # combine with all the simulate reuslts.
+    # row: sample, col: gene
+    CNVs <- rbind(CNVs, one_gene_simulate[1,])
+    Genes <- rbind(Genes, one_gene_simulate[2,])
+  }
+  
+  
+  Gene_name = paste('GENE',seq(1:n_genes))
   Sample_name = paste('Sample', seq(1:n_people))
   
-  CNVs <- data.table(Gene_name, CNVs)
-  Genes <- data.table(Gene_name, Genes)
+  CNVs <- data.table(GENE = Gene_name,CNVs)
+  colnames(CNVs) <- c('GENE',Sample_name)
   
-  colnames(CNVs) <- c('GENE', Sample_name)
-  colnames(Genes) <- c('GENE', Sample_name)
+  Genes <- data.table(GENE = Gene_name,Genes)
+  colnames(Genes) <- c('GENE',Sample_name)
+
   
   simData <- list()
   simData[[1]] <- CNVs
   simData[[2]] <- Genes
   simData[[3]] <- correlation
+  
+  cat("Number of genes:", n_genes, '\n')
+  cat("Number of people", n_people, '\n')
   return(simData)
 }
  
 ## data[[1]]: CNV, data[[2]]: Gene
-data <- dataSimulation(n_people = 1)
+data <- dataSimulation(n_people = 100, n_genes = 300)
 
 # iGC
 
@@ -71,9 +81,12 @@ require(iGC)
 # cnv data should be -1, 0, 1 (for iGC)
 cnv = data[[1]]
 gene = data[[2]]
-cnv[,2:30] = 1
-cnv[,31:40] = 0
-cnv[,41:51] = -1
+temp = which(cnv < cnv_threhold, arr.ind = T)
+cnv[temp] = 0
+cnv[temp[3001:dim(temp)[1],]] = -1
+cnv[which(cnv >= cnv_threhold, arr.ind = T)] = 1
+cnv[,1] =  paste('GENE',seq(1:300))
+
 result <- find_cna_driven_gene(gene_cna = cnv, gene_exp = gene)
 
 
